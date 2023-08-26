b3e01183de8956c0e009d903e69197e4b300d687
TinyMarcus <ilyasov9999@gmail.com>
2023-08-26 17:53:53 +0300

    [6] Added comments in models package for swagger generator

--- src/dynamic-user-segmentation-service/internal/models/segment.go
+++ src/dynamic-user-segmentation-service/internal/models/segment.go
@@ -2,10 +2,12 @@ package models
 
 import "database/sql"
 
+// Segment model info
+// @Description Информация о сегменте
 type Segment struct {
-	Id          int    `json:"id,omitempty"`
-	Slug        string `json:"slug"`
-	Description string `json:"description,omitempty"`
+	Id          int    `json:"id,omitempty"`          // Идентификатор сегмента
+	Slug        string `json:"slug"`                  // Название сегмента
+	Description string `json:"description,omitempty"` // Описание сегмента
 }
 
 type UserSegment struct {
@@ -14,24 +16,32 @@ type UserSegment struct {
 	DeadlineDate sql.NullString `json:"deadline_date,omitempty"`
 }
 
+// CreateOrUpdateSegmentDto model info
+// @Description Информация о сегменте при создании
 type CreateOrUpdateSegmentDto struct {
-	Slug        string `json:"slug"`
-	Description string `json:"description,omitempty"`
+	Slug        string `json:"slug"`                  // Название сегмента
+	Description string `json:"description,omitempty"` // Описание сегмента
 }
 
+// SegmentWithDeadlineDate model info
+// @Description Информация о сегментах с датой отключения пользователя от сегмента
 type SegmentWithDeadlineDate struct {
-	Slug         string `json:"slug"`
-	DeadlineDate string `json:"deadline_date,omitempty"`
+	Slug         string `json:"slug"`                    // Название сегмента
+	DeadlineDate string `json:"deadline_date,omitempty"` // Дата отключения пользователя от сегмента
 }
 
+// ChangeUserSegmentsDto model info
+// @Description Информация о добавляемых и удаляемых сегментах пользователя
 type ChangeUserSegmentsDto struct {
-	AddToUser    []SegmentWithDeadlineDate `json:"add_to_user"`
-	TakeFromUser []string                  `json:"take_from_user"`
+	AddToUser    []SegmentWithDeadlineDate `json:"add_to_user"`    // Сегменты, которые будут добавляться пользователю (с датами отключения)
+	TakeFromUser []string                  `json:"take_from_user"` // Сегменты, которые будут удаляться у пользователя
 }
 
+// UsersActiveSegments model info
+// @Description Информация об активных сегментах пользователя
 type UsersActiveSegments struct {
-	UserId   int                       `json:"user_id"`
-	Segments []SegmentWithDeadlineDate `json:"segments"`
+	UserId   int                       `json:"user_id"`  // Идентификатор пользователя
+	Segments []SegmentWithDeadlineDate `json:"segments"` // Список активных сегментов
 }
 
 func ConvertUserSegmentToUsersActiveSegments(userId int, userSegments []*UserSegment) *UsersActiveSegments {
