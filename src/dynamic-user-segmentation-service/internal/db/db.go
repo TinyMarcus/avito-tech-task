ce328352a29fd055a63a7ecb8fa8c87e65ba1e18
TinyMarcus <ilyasov9999@gmail.com>
2023-08-27 10:14:49 +0300

    [7] Changed sql/database to sqlx for testing

--- src/dynamic-user-segmentation-service/internal/db/db.go
+++ src/dynamic-user-segmentation-service/internal/db/db.go
@@ -1,20 +1,20 @@
 package db
 
 import (
-	"database/sql"
 	"fmt"
+	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 	"os"
 )
 
-func CreateConnection() *sql.DB {
+func CreateConnection() *sqlx.DB {
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
 		os.Getenv("HOST"),
 		os.Getenv("USER"),
 		os.Getenv("PASSWORD"),
 		os.Getenv("NAME"),
 		os.Getenv("DBPORT"))
-	db, err := sql.Open(os.Getenv("TYPE"), dsn)
+	db, err := sqlx.Open(os.Getenv("TYPE"), dsn)
 	if err != nil {
 		panic(err)
 	}
